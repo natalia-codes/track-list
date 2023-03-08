45a4f7496802fa807931b1dacf951f1573a02dc7
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2023-03-07 18:43:39 -0800

    locations + ip uuid format

---
+++ mapses.py
@@ -0,0 +1,10 @@
+import gmaps 
+
+with open('apikey.txt') as f:
+    api_key = f.readline()
+    f.close
+
+gmaps.configure(api_key='')
+
+new_york_coordinates = (40.75, -74.00)
+gmaps.figure(center=new_york_coordinates, zoom_level=12)
